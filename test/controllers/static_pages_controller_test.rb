21e32ebc626984744b27239df37cdf4da0e61c0e
Luis Miguel Delgado <lmd411@gmail.com>
2015-02-19 10:13:11 -0600

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
+
   test "should get home" do
     get :home
     assert_response :success
@@ -23,4 +24,9 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
+  end
 end
